86bc5cc5aa72da6305981c28141ce6be6243cde9
varun <coldvks@gmail.com>
2026-07-12 17:22:17 +0530

    add Booking page

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,11 +1,303 @@
+import { useState } from "react";
+import {useAuth} from '../context/AuthContext';
 
 function BookingPage() {
+  const currentUser = useAuth();
+  const [trainDetails, setTrainDetails] = useState(null);
+  const [selectedClass, setSelectedClass] = useState('');
+  const [availableClasses, setAvailableClasses] = useState([]);
+  const [classPrice, setClassPrice] = useState({});
+  const [passengers, setPassengers] = useState([{
+    name: '',
+    age: '',
+    gender: '',
+    berth: 'No preference'
+  }]);
+  const [contactInfo, setContactInfo] = useState({
+    email: currentUser?.email || '',
+    phone: ''
+  })
+
+  // handle class change for selected class
+  const handleClassChange = (e) => {
+    setSelectedClass(e.target.value);
+  }
+
+  // update passenger details
+  const updatePassenger = (index, field, value) => {
+    const updatedPassengers = [...passengers];
+    updatedPassengers[index][field] = value;
+    setPassengers[updatedPassengers];
+  }
+
+  // remove passenger
+  const removePassenger = (index) => {
+    if (passengers.length > 1) {
+      const updatedPassengers = [...passengers];
+      updatedPassengers.splice(index, 1);
+      setPassengers(updatedPassengers);
+    }
+  }
+
+  // add passenger
+  const addPassenger = () => {
+    setPassengers([...passengers, {
+      name: '',
+      age: '',
+      gender: '',
+      berth: 'No preference'
+    }])
+  }
+
+  // fare calculation
+  const calculateTotalFare = () => {
+    const passengerCount = passengers.length;
+    
+    // get the fare for the selected class
+    const baseFarePerPassenger = classPrice[selectedClass] || 0;
+    const totalBaseFare = baseFarePerPassenger * passengerCount;
+
+    //calculate addtional charges
+    const gst = Math.round(totalBaseFare * 0.05); // 5% gst
+    const convenienceFee = 30; // fixed
+    const cateringCharge = selectedClass === 'Executive Class' ? 150 * passengerCount: 120 * passengerCount;
+
+    return {
+      baseFare: totalBaseFare,
+      gst,
+      convenienceFee,
+      cateringCharge,
+      total: totalBaseFare + gst + convenienceFee + cateringCharge
+    }
+  }
+
+  const fareDetails = calculateTotalFare();
+
   return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
-      <h2>Booking Page</h2>
-      <p>Select your train and proceed with booking!</p>
+    <div className={styles.container}>
+      {/* Train Details */}
+      <h2>Book Your Train Ticket</h2>
+      <div className={styles.trainSummary}>
+        <h3>Tain Dtails</h3>
+        <div className={styles.detailsGrid}>
+          <div>
+            <strong>Train Number: </strong> {trainDetails.trainNumber}
+          </div>
+          <div>
+            <strong>Train Name: </strong> {trainDetails.trainName}
+          </div>
+          <div>
+            <strong>From: </strong> {trainDetails.from}
+          </div>
+          <div>
+            <strong>To: </strong> {trainDetails.to}
+          </div>
+          <div>
+            <strong>Date: </strong> {trainDetails.date}
+          </div>
+          <div>
+            <strong>Departure: </strong> {trainDetails.departure_time}
+          </div>
+          <div>
+            <strong>Arrival: </strong> {trainDetails.arrival_time}
+          </div>
+          <div>
+            <strong>Duration: </strong> {trainDetails.duration}
+          </div>
+          <div>
+            <strong>Class: </strong>
+            <select
+              value={selectedClass}
+              onChange={handleClassChange}
+              style={{
+                marginLeft: "10px",
+                padding: "3px",
+                borderRadius: "5px",
+              }}
+            >
+              {availableClasses.map((cls) => (
+                <option key={cls} value={cls}>
+                  {" "}
+                  {cls} (₹{classPrice[cls]})
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <strong>Quota</strong> {trainDetails.quota}
+          </div>
+        </div>
+      </div>
+
+      {/* Passenger Details */}
+      <form>
+        <div className={styles.passengerSection}>
+          <h3>Passenger Details</h3>
+          {passengers.map((passenger, index) => (
+            <div key={index} className={styles.passengerCard}>
+              <h4>Passenger {index + 1}</h4>
+              {/* Name */}
+              <div className={styles.inputGroup}>
+                <label htmlFor="">
+                  Name:
+                  <input
+                    type="text"
+                    value={passenger.name}
+                    onChange={(e) =>
+                      updatePassenger(index, "name", e.target.value)
+                    }
+                    required
+                    placeholder="Enter full name as per ID"
+                  />
+                </label>
+              </div>
+              {/* Age */}
+              <div className={styles.inputGroup}>
+                <label htmlFor="">
+                  Age:
+                  <input
+                    type="number"
+                    value={passenger.age}
+                    onChange={(e) =>
+                      updatePassenger(index, "age", e.target.value)
+                    }
+                    required
+                    placeholder="Enter your age in years"
+                    min={1}
+                    max={110}
+                  />
+                </label>
+              </div>
+              {/* Gender */}
+              <div className={styles.inputGroup}>
+                <label htmlFor="">
+                  Gender:
+                  <select
+                    name="gender"
+                    value={passenger.gender}
+                    onChange={(e) =>
+                      updatePassenger(index, "gender", e.target.value)
+                    }
+                  >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </label>
+              </div>
+              {/* Berth Preference */}
+              <div className={styles.inputGroup}>
+                <label htmlFor="">
+                  Berth Preference:
+                  <select
+                    name="bithPreference"
+                    value={passenger.berth}
+                    onChange={(e) =>
+                      updatePassenger(index, "gender", e.target.value)
+                    }
+                  >
+                    <option value="lower">Lower</option>
+                    <option value="middle">Middle</option>
+                    <option value="upper">Upper</option>
+                  </select>
+                </label>
+              </div>
+              {/* Button to remove passenger (if more than 1 passenger) */}
+              {passenger.length > 1 && (
+                <button
+                  type="button"
+                  className={styles.removeButton}
+                  onClick={() => removePassenger(index)}
+                >
+                  Remove
+                </button>
+              )}
+            </div>
+          ))}
+          <button
+            type="button"
+            className={styles.addButton}
+            onClick={addPassenger}
+          >
+            Add passenger
+          </button>
+        </div>
+
+        {/* information/contact section */}
+        <div className={styles.contactSection}>
+          <h3>Contact Information</h3>
+          {/* Email */}
+          <div className={styles.inputGroup}>
+            <label htmlFor="">
+              Email:
+              <input
+                type="email"
+                value={contactInfo.email}
+                onChange={(e) => handleContactChange("email", e.target.value)}
+                required
+                placeholder="Enter email for e-ticket and updates"
+              />
+            </label>
+          </div>
+          {/* phone */}
+          <div className={styles.inputGroup}>
+            <label htmlFor="">
+              Phone:
+              <input
+                type="tel"
+                value={contactInfo.phone}
+                onChange={(e) => handleContactChange("phone", e.target.value)}
+                required
+                pattern="[0-9]{10}"
+                title="Phone number must be of 10 digits"
+                placeholder="Enter 10 digit mobile number"
+              />
+            </label>
+          </div>
+        </div>
+
+        {/* payment section */}
+        <div className={styles.payementSection}>
+          <h3>Payement Summary</h3>
+          <div className={style.paymentDetails}>
+            <div>
+              <span>Base Fare ({selectedClass} x {passengers.length}):</span>
+              <span>₹{fareDetails.baseFare}</span>
+            </div>
+            <div>
+              <span>Catering Charges:</span>
+              <span>₹{fareDetails.cateringCharge}</span>
+            </div>
+            <div>
+              <span>GST (5%):</span>
+              <span>₹{fareDetails.gst}</span>
+            </div>
+            <div>
+              <span>Convenience Fee:</span>
+              <span>₹{fareDetails.convenienceFee}</span>
+            </div>
+            <div className={styles.totalAmount}>
+              <span>Total Amount:</span>
+              <span>₹{fareDetails.total}</span>
+            </div>
+          </div>
+        </div>
+
+        {/* terms section */}
+        <div className={styles.termsSection}>
+          <label htmlFor="">
+            <input type="checkbox" />
+            I agree to the Terms and conditions with cancellation policy.
+          </label>
+        </div>
+
+        {/* Payment proceed button */}
+        <div className={styles.buttonGroup}>
+          <button type="submit" className={styles.payButton}>Proceed to payment</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default BookingPage
+export default BookingPage;
